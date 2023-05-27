b5659b983bfb1b478f84778a84b93979d80182b7
Eui9179 <outdark97@gmail.com>
2023-05-27 22:12:52 +0900

    feat: redis-publisher-and-subscriber

---
+++ src/main/java/site/leui/chat_example/redis/service/RedisPublisher.java
@@ -0,0 +1,17 @@
+package site.leui.chat_example.redis.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.stereotype.Service;
+import site.leui.chat_example.chat.dto.ChatMessage;
+
+@RequiredArgsConstructor
+@Service
+public class RedisPublisher {
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public void publish(ChannelTopic topic, ChatMessage message) {
+        redisTemplate.convertAndSend(topic.getTopic(), message);
+    }
+}
---
+++ src/main/java/site/leui/chat_example/redis/service/RedisSubscriber.java
@@ -0,0 +1,34 @@
+package site.leui.chat_example.redis.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Service;
+import site.leui.chat_example.chat.dto.ChatMessage;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class RedisSubscriber implements MessageListener {
+    private final ObjectMapper objectMapper;
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final SimpMessageSendingOperations messageTemplate;
+    
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        try {
+            String publishMessage = redisTemplate
+                    .getStringSerializer()
+                    .deserialize(message.getBody());
+            ChatMessage roomMessage = objectMapper.readValue(publishMessage, ChatMessage.class);
+            messageTemplate.convertAndSend("/sub/chat/room/" + roomMessage.getRoomId(), roomMessage);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
